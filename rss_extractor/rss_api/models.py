331d40423c0a7c0acbab0dbc25e1ee8f721d91a0
AyoBakre <ayomidebakre@gmail.com>
2023-12-05 16:13:36 +0100

    added model

--- rss_extractor/rss_api/models.py
+++ rss_extractor/rss_api/models.py
@@ -1,3 +1,9 @@
+# rss_api/models.py
 from django.db import models
 
-# Create your models here.
+
+class ParsedData(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    last_parsed = models.DateTimeField(auto_now=True)
